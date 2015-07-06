799fd117ced3e9bbbe2515dd1d90ae21d2abe14b
begreattoplay <letmepicksomething@gmail.com>
2015-07-06 17:17:59 -0500

    C# Day 1: More examples in First Project

--- FirstProject/FirstProject/Program.cs
+++ FirstProject/FirstProject/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using FirstProject.Helpers;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +12,53 @@ namespace FirstProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Troop!");
-            //DoSomething();
+            //****Checking for file example****
+            //if (File.Exists(@"HelloWorld.txt"))
+            //{
+            //    Console.WriteLine("File exists!!");
+            //}
+
+            //****Convert string to number example****
+            Console.WriteLine("Enter the first number:");
+            int firstNum = 0;
+            bool firstNumIsNumber = int.TryParse(Console.ReadLine(), out firstNum);
+
+            Console.WriteLine("Enter the second number:");
+            int secondNum = 0;
+            bool secondNumIsNumber = int.TryParse(Console.ReadLine(), out secondNum);
+
+            //display result
+            if (firstNumIsNumber && secondNumIsNumber)
+            {
+                int result = firstNum + secondNum;
+                Console.WriteLine("The result is " + result.ToString());
+            }
+            else
+            {
+                Console.WriteLine("You did not enter valid numbers!");
+            }
+
+            //****Switch Statement Example****
+            //DayOfWeek dayOfWeek = DateTime.Now.DayOfWeek;
+
+            //string dinner = "";
+
+            //switch (dayOfWeek)
+            //{
+            //    case DayOfWeek.Monday:
+            //        dinner = "Steak";
+            //        break;
+            //    case DayOfWeek.Tuesday:
+            //        dinner = "Chicken";
+            //        break;
+            //    default:
+            //        dinner = "Pasta";
+            //        break;
+            //}
+
+            //Console.WriteLine(dinner);
+
+            Console.ReadLine();
         }
     }
 }
