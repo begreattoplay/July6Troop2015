8f70e7264b39cf915e8b0ddd0bec2a2f0921f4e6
begreattoplay <letmepicksomething@gmail.com>
2015-07-14 09:01:09 -0500

    Cat Counter and Prevent XSS homework samples

---
+++ CatCounter/CatCounter/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //7 instances of cat
+            string message = "cat somewhere there is a cat with a cat in the hat next to a cat and another cat named cat i wish I was a cat";
+
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+            int result = CountCats2(message);
+            timer.Stop();
+
+            Console.WriteLine("Found {0} instances of 'cat' and it took {1} milliseconds", result, timer.Elapsed.TotalMilliseconds);
+            Console.ReadLine();
+        }
+
+        static int CountCats(string message)
+        {
+            //About 1.5 seconds
+            return message.Split().Count(s => s == "cat");
+        }
+
+        static int CountCats2(string message)
+        {
+            //About .5 seconds
+            int count = 0;
+            int i = 0;
+            while ((i = message.IndexOf("cat", i)) != -1)
+            {
+                i += 3;
+                count++;
+            }
+
+            return count;
+        }
+    }
+}
---
+++ PreventXSS/PreventXSS/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace PreventXSS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string test = "<b>hello</b><script>evil</script>";
+
+            string result = Security.SanitizeHTML(test, new string[] {"b"});
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+
+    public class Security
+    {
+        public static string SanitizeHTML(string text, params string[] validTags)
+        {
+            string sanitized = HttpUtility.HtmlEncode(text);
+
+            foreach(var tag in validTags)
+            {
+                sanitized = sanitized.Replace("&lt;" + tag + "&gt;", "<" + tag + ">");
+                sanitized = sanitized.Replace("&lt;/" + tag + "&gt;", "<" + tag + ">");
+                
+            }
+
+            return sanitized;
+        }
+    }
+}
